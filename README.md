c351d29cc864a283aee89c4d88aa4d1ca0f7a926
Manas <manaschougule2019@gmail.com>
2023-10-22 17:07:30 +0530

    Set up initial structure

---
+++ README.md
@@ -0,0 +1,17 @@
+# ReactBlog
+Objective is to learn React hooks by building a dynamic blogging app, learn modern functional components and implement app using different hooks like useState() and useReducer() by creating & deleting blogs and more.
+
+# Getting Started with Create React App
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+The page will reload when you make changes.\
+You may also see any lint errors in the console.
+
---
+++ src/Components/Blog.js
@@ -0,0 +1,54 @@
+//Blogging App using Hooks
+
+export default function Blog(){
+
+
+    function handleSubmit(e){
+        e.preventDefault();
+    }
+
+    return(
+        <>
+        <h1>Write a Blog!</h1>
+        <div className="section">
+
+        {/* Form for to write the blog */}
+            <form onSubmit={handleSubmit}>
+                <Row label="Title">
+                        <input className="input"
+                                placeholder="Enter the Title of the Blog here.."
+                        />
+                </Row >
+
+                <Row label="Content">
+                        <textarea className="input content"
+                                placeholder="Content of the Blog goes here.."
+                        />
+                </Row >
+         
+                <button className = "btn">ADD</button>
+            </form>
+                     
+        </div>
+
+        <hr/>
+
+        {/* Section where submitted blogs will be displayed */}
+        <h2> Blogs </h2>
+
+        
+        </>
+        )
+    }
+
+//Row component to introduce a new row section in the form
+function Row(props){
+    const{label} = props;
+    return(
+        <>
+        <label>{label}<br/></label>
+        {props.children}
+        <hr />
+        </>
+    )
+}
