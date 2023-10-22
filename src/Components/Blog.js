42017ed99c90a38d31e7fa5a1ac83d3555bd0ab7
Manas <manaschougule2019@gmail.com>
2023-10-22 21:28:21 +0530

    pass object to useState() & add delete blog funtionality

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -3,16 +3,24 @@ import { useState } from "react";
 
 export default function Blog(){
 
-    const [title,setTitle] = useState("");
-    const [content,setContent] = useState("");
+    // const [title,setTitle] = useState("");
+    // const [content,setContent] = useState("");
+    const [formData,setFormData]=useState({ title:"", content:"" }); // title & content gets updated at same time hence combined into single object
     const [blogs, setBlogs] =  useState([]);
 
     function handleSubmit(e){
         e.preventDefault();
 
-        setBlogs([{title,content}, ...blogs]);
+        setBlogs([{title: formData.title, content: formData.content}, ...blogs]);  
+        // setTitle("");
+        // setContent("");
+        setFormData({title: "" , content: ""})
         console.log(blogs); 
 
+    }
+
+    function removeBlog(i){
+        setBlogs(blogs.filter((blog,index)=> i!==index));
     }
 
     return(
@@ -25,14 +33,16 @@ export default function Blog(){
                 <Row label="Title">
                         <input className="input"
                                 placeholder="Enter the Title of the Blog here.."
-                                onChange = {(e) => setTitle(e.target.value)}
+                                value={formData.title}
+                                onChange = {(e) => setFormData({title : e.target.value , content: formData.content})}
                         />
                 </Row >
 
                 <Row label="Content">
                         <textarea className="input content"
                                 placeholder="Content of the Blog goes here.."
-                                onChange = {(e) => setContent(e.target.value)}
+                                value={formData.content}
+                                onChange = {(e) => setFormData({title : formData.title , content : e.target.value})}
                         />
                 </Row >
          
@@ -47,9 +57,16 @@ export default function Blog(){
         <h2> Blogs </h2>
         {blogs.map((blog,i) => (
             <div className="blog">
+
                 <h3>{blog.title}</h3>
                 <hr/>
                 <p>{blog.content}</p>
+                <div className="blog-btn">
+                    <button onClick={()=>removeBlog(i)} className="btn remove">
+                        Delete
+                    </button>
+                </div>
+
             </div>
         ))}
         
